8388d60bca0cc6b51efb5ae813bf6f84b65e3f3d
Khedr05 <sherifkhadr1@gmail.com>
2024-11-04 00:11:18 +0200

    fav implement

---
+++ SkyRadar/app/src/main/java/com/example/skyradar/favouritesLocations/view/FavDetailsHourlyAdapter.kt
@@ -0,0 +1,76 @@
+package com.example.skyradar.favouritesLocations.view
+
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.skyradar.Helpers.formatTimestamp
+import com.example.skyradar.Helpers.getHourFromUnixTime
+import com.example.skyradar.R
+import com.example.skyradar.model.WeatherList
+import java.util.Calendar
+
+class FavDetailsHourlyAdapter : ListAdapter<WeatherList, FavDetailsHourlyAdapter.HourlyWeatherViewHolder>(HourlyWeatherDiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HourlyWeatherViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_hourly, parent, false)
+        return HourlyWeatherViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: HourlyWeatherViewHolder, position: Int) {
+        val animation = AnimationUtils.loadAnimation(holder.itemView.context, R.anim.scale_in_animation)
+        holder.itemView.startAnimation(animation)
+        holder.bind(getItem(position))
+    }
+
+    fun submitTodayWeather(list: List<WeatherList>) {
+        // Filter the list to only include today's weather
+        val todayList = list.filter { isToday(it.dt) }
+        submitList(todayList)
+    }
+
+    inner class HourlyWeatherViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val tvDegreeDayHour: TextView = itemView.findViewById(R.id.tv_degree_day_hour)
+        private val timeHour: TextView = itemView.findViewById(R.id.time_hour)
+        private val imvWeatherHour: ImageView = itemView.findViewById(R.id.imv_weather_hour)
+
+        fun bind(weather: WeatherList) {
+            tvDegreeDayHour.text = weather.main.temp.toString()
+            timeHour.text = formatTimestamp(weather.dt)
+            val hour = getHourFromUnixTime(weather.dt)
+            val icon = when (hour) {
+                2, 5, 8, 11  -> R.drawable.ic_day_hour
+                else -> R.drawable.ic_night_hour
+            }
+            imvWeatherHour.setImageResource(icon)
+        }
+    }
+
+    class HourlyWeatherDiffCallback : DiffUtil.ItemCallback<WeatherList>() {
+        override fun areItemsTheSame(oldItem: WeatherList, newItem: WeatherList): Boolean {
+            return oldItem.dt == newItem.dt
+        }
+
+        override fun areContentsTheSame(oldItem: WeatherList, newItem: WeatherList): Boolean {
+            return oldItem == newItem
+        }
+    }
+}
+
+// Helper function to check if a timestamp is from today
+fun isToday(timestamp: Long): Boolean {
+    val calendar = Calendar.getInstance()
+    val today = Calendar.getInstance()
+
+    calendar.timeInMillis = timestamp * 1000 // assuming timestamp is in seconds
+    return today.get(Calendar.YEAR) == calendar.get(Calendar.YEAR) &&
+            today.get(Calendar.DAY_OF_YEAR) == calendar.get(Calendar.DAY_OF_YEAR)
+}
+
+
--- SkyRadar/app/src/main/java/com/example/skyradar/favouritesLocations/view/FavouritesLocationsDetailsFragment.kt
+++ SkyRadar/app/src/main/java/com/example/skyradar/favouritesLocations/view/FavouritesLocationsDetailsFragment.kt
@@ -2,33 +2,134 @@
 package com.example.skyradar.favouritesLocations.view
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.skyradar.Helpers.formatTimestamp
+import com.example.skyradar.Helpers.getCurrentDate
 import com.example.skyradar.R
+import com.example.skyradar.database.AlarmLocalDataSourceImpl
+import com.example.skyradar.database.LocationLocalDataSourceImpl
+import com.example.skyradar.favouritesLocations.viewmodel.FavouritesLocationsViewModel
+import com.example.skyradar.favouritesLocations.viewmodel.FavouritesLocationsViewModelFactory
+import com.example.skyradar.home.view.DailyAdapter
+import com.example.skyradar.home.view.HourlyAdapter
+import com.example.skyradar.home.viewmodel.HomeFactory
+import com.example.skyradar.home.viewmodel.HomeViewModel
 import com.example.skyradar.model.DatabasePojo
+import com.example.skyradar.model.RepositoryImpl
+import com.example.skyradar.network.RemoteDataSourceImpl
+import com.example.skyradar.network.RetrofitInstance
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.launch
 
 class FavouritesLocationsDetailsFragment : Fragment() {
+
+    private lateinit var dailyAdapter: DailyAdapter
+    private lateinit var hourlyAdapter: HourlyAdapter
+    private lateinit var rvHourly: RecyclerView
+    private lateinit var rvDaily: RecyclerView
+    private lateinit var favViewModel: FavouritesLocationsViewModel
+    private val uiElements: MutableMap<String, TextView> = mutableMapOf()
+    private lateinit var rmFromFavouritesButton: ImageView
+
+    private var selectedLocation : DatabasePojo? = null
+    private var cityWeatherDetails: DatabasePojo? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_favourites_locations_details, container, false)
 
         // Retrieve the data
-        val selectedLocation = arguments?.getSerializable("selected_location") as? DatabasePojo
-
-        // Use selectedLocation to update UI or perform actions
-
-        if (selectedLocation != null) {
-            Log.i("FavouritesLocationsDetailsFragment", "Selected Location: ${selectedLocation.Weather.name}")
-            Log.i("FavouritesLocationsDetailsFragment", "Selected Location: ${selectedLocation.Weather.main?.temp}")
-        } else {
-            Log.i("FavouritesLocationsDetailsFragment", "Selected Location is null")
-        }
-
+         selectedLocation = arguments?.getSerializable("selected_location") as? DatabasePojo
 
         return view
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initializeUI(view)
+        initializeViewModel()
+        setupRecyclerView()
+        showDetails()
+        //observeViewModel()
+
+    }
+
+    private fun showDetails() {
+        uiElements["date"]?.text = getCurrentDate()
+        uiElements["cityName"]?.text = selectedLocation?.Weather?.name
+        uiElements["currentTempValue"]?.text = selectedLocation?.Weather?.main?.temp.toString()
+        uiElements["weatherValue"]?.text = selectedLocation?.Weather?.weather?.get(0)?.description?.capitalize()
+        uiElements["humidityValue"]?.text = selectedLocation?.Weather?.main?.humidity.toString()
+        uiElements["pressureValue"]?.text = selectedLocation?.Weather?.main?.pressure.toString()
+        uiElements["tempMaxValue"]?.text = selectedLocation?.Weather?.main?.tempMax.toString()
+        uiElements["tempMinValue"]?.text = selectedLocation?.Weather?.main?.tempMin.toString()
+        uiElements["windSpeedValue"]?.text = selectedLocation?.Weather?.wind?.speed.toString()
+        uiElements["cloudsValue"]?.text = selectedLocation?.Weather?.clouds?.all.toString()
+        uiElements["sunriseValue"]?.text = formatTimestamp(selectedLocation?.Weather?.sys?.sunrise)
+        uiElements["sunsetValue"]?.text = formatTimestamp(selectedLocation?.Weather?.sys?.sunset)
+        selectedLocation?.Forecast?.list?.let { dailyAdapter.submitWeatherList(it) }
+        selectedLocation?.Forecast?.list?.let { hourlyAdapter.submitTodayWeather(it) }
+    }
+
+    private fun initializeUI(view: View) {
+        rmFromFavouritesButton = view.findViewById(R.id.btn_favourites)
+        rvHourly = view.findViewById(R.id.rv_hourly_degrees)
+        rvDaily = view.findViewById(R.id.rv_detailed_days)
+        uiElements["cityName"] = view.findViewById(R.id.tv_city_name)
+        uiElements["date"] = view.findViewById(R.id.tv_date)
+        uiElements["currentTempValue"] = view.findViewById(R.id.tv_current_degree)
+        uiElements["weatherValue"] = view.findViewById(R.id.tv_weather_status)
+        uiElements["tempMaxValue"] = view.findViewById(R.id.tv_temp_max)
+        uiElements["tempMinValue"] = view.findViewById(R.id.tv_temp_min)
+        uiElements["humidityValue"] = view.findViewById(R.id.tv_humidity_value)
+        uiElements["pressureValue"] = view.findViewById(R.id.tv_pressure_value)
+        uiElements["windSpeedValue"] = view.findViewById(R.id.tv_wind_value)
+        uiElements["cloudsValue"] = view.findViewById(R.id.tv_cloud_value)
+        uiElements["sunriseValue"] = view.findViewById(R.id.tv_sunrise_value)
+        uiElements["sunsetValue"] = view.findViewById(R.id.tv_sunset_value)
+
+        rmFromFavouritesButton.setOnClickListener {
+            // Handle the button click event
+            if (selectedLocation != null) {
+                // Save cityWeatherDetails to favorites or handle the favorite action
+                favViewModel.removeFavoriteLocation(selectedLocation!!)
+                showSnackbar("Removed From Favorites")
+            } else {
+                showSnackbar("Error: No weather details available to remove.")
+            }
+        }
+    }
+
+    private fun initializeViewModel() {
+        val factory = FavouritesLocationsViewModelFactory(
+            RepositoryImpl(
+                RemoteDataSourceImpl.getInstance(RetrofitInstance.retrofit),
+                LocationLocalDataSourceImpl.getInstance(requireContext()),
+                AlarmLocalDataSourceImpl.getInstance(requireContext()))
+        )
+        favViewModel = ViewModelProvider(this, factory).get(FavouritesLocationsViewModel::class.java)
+    }
+
+    private fun setupRecyclerView() {
+        hourlyAdapter = HourlyAdapter()
+        rvHourly.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        rvHourly.adapter = hourlyAdapter
+        dailyAdapter = DailyAdapter()
+        rvDaily.layoutManager = LinearLayoutManager(requireContext())
+        rvDaily.adapter = dailyAdapter
+    }
+
+
+
+    private fun showSnackbar(message: String) {
+        Snackbar.make(requireView(), message, Snackbar.LENGTH_LONG).show()
+    }
 }
